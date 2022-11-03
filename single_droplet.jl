db802ed58543fbffad25b007c1956f7b448cdf2e
Kyle Chan <kylewingchan@gmail.com>
2022-11-04 01:52:50 +0800

    .

--- single_droplet.jl
+++ single_droplet.jl
@@ -9,10 +9,10 @@ part = Particle()
 sysPara = SysPara()
 
 
-sysPara.nx = 2400
-sysPara.ny = 2400
-sysPara.dx = 0.05
-sysPara.dy = 0.05
+sysPara.nx = 500
+sysPara.ny = 500
+sysPara.dx = 0.2
+sysPara.dy = 0.2
 
 sysPara.dt = 0.001
 
@@ -26,14 +26,17 @@ part.ω0 = 1
 part.Dr = 0.0
 part.D = 0.1
 # part.α =5 * 10^2
-part.α = -180
+part.α = -5
 # part.α = 0
 
 sysPara.npoly = 180 #! Must be a multiple of 4
-sysPara.Nstep = 100_000
+sysPara.Nstep = 10_000
+
+sysPara.flow = [source, rotlet]
+# sysPara.flow = ["off"]
 
 @time u, all_pos, all_F, flow = Simulation(sysPara, part)
 
-
-savename="raw/v$(part.v0)_w0$(part.ω0)/D$(part.D)_a$(part.α)_dx$(sysPara.dx)_nx$(sysPara.nx)_N$(sysPara.Nstep).jld2"
+savename = "raw/v$(part.v0)_w0$(part.ω0)_flow/D$(part.D)_a$(part.α)_dx$(sysPara.dx)_nx$(sysPara.nx)_N$(sysPara.Nstep).jld2"
+# savename="raw/v$(part.v0)_w0$(part.ω0)/D$(part.D)_a$(part.α)_dx$(sysPara.dx)_nx$(sysPara.nx)_N$(sysPara.Nstep).jld2"
 data = Dict("field"=>u, "pos"=>all_pos, "force"=>all_F, "flow"=>flow)
