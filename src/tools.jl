d1ce07e0441a6b74e45ed5082e3e973a53da670d
kylechanphy <kylewingchan@gmail.com>
2022-11-04 16:07:03 +0800

    change save dir

--- src/tools.jl
+++ src/tools.jl
@@ -1,6 +1,6 @@
 function savedata(field, all_pos, all_F, flow, part, sysPara)
     if sysPara.flow
-        savedir = "raw/v$(part.v0)_w0$(part.ω0)_flow/D$(part.D)_a$(part.α)_dx$(sysPara.dx)_nx$(sysPara.nx)_N$(sysPara.Nstep)"
+        savedir = "raw/v$(part.v0)_w0$(part.ω0)/flow_D$(part.D)_a$(part.α)_dx$(sysPara.dx)_nx$(sysPara.nx)_N$(sysPara.Nstep)"
     else
         savedir = "raw/v$(part.v0)_w0$(part.ω0)/D$(part.D)_a$(part.α)_dx$(sysPara.dx)_nx$(sysPara.nx)_N$(sysPara.Nstep)"
     end
